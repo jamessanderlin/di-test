7a1de414f0b64306e65c6b5ab31d094800e4a783
James <james.sanderlin@gmail.com>
2021-04-29 18:27:13 -0500

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace di_test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly MyDbContext _myDbContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, MyDbContext myDbContext)
+        {
+            _logger = logger;
+            _myDbContext = myDbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace di_test
+{
+    public class MyDbContext : DbContext, IDisposable
+    {
+        public MyDbContext()
+        {
+            Console.WriteLine("Creating MyDbContext");
+        }
+
+        public override void Dispose()
+        {
+            Console.WriteLine("Disposing MyDbContext");
+            base.Dispose();
+        }
+    }
+}
